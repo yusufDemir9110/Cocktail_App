1e5bda4d05a954662f4f34d95d055be7027e95f7
yusuf Demir <yusuf.ferrum@gmail.com>
2023-01-19 15:39:27 +0100

    cocktail detail

--- src/components/GetCategories.jsx
+++ src/components/GetCategories.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 const GetCategories = () => {
     const [data,setData]=useState([])
+    const [drinksData, setDrinksData] = useState([])
     const getCategories =()=>{
         fetchData();
     }
@@ -11,16 +13,43 @@ const GetCategories = () => {
       );
       const json = await res.json();
       setData(json.drinks);
+      console.log(data)
+    };
+    const getDrinks =(data)=>{
+        const filter = data.strCategory.replace(" ","_")
+        console.log(filter)
+        fetchData2(filter);
+    }
+    const fetchData2 = async (filter) => {
+      const res = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filter}`,
+      );
+      const json2 = await res.json();
+      setDrinksData(json2.drinks);
+      console.log(drinksData)
     };
     
   return (
     <div>
         <h3 onClick={()=>getCategories()}>Get the categories</h3>
-        <div>
+        <ul>
             {
-            data.map(data=>(<div>{data.strCategory}</div>))
+           data.map(data=>(
+                <li key={data.strCategory} onClick={()=>getDrinks(data)}>
+                        {data.strCategory}
+                </li>))
             }
-        </div>
+        </ul>
+        <ul>
+            {drinksData&&
+            drinksData.map(data=>(
+                <li key={data.idDrink}>
+                    <Link to={"/cocktail-detail"} state={data.idDrink}>
+                        {data.strDrink}
+                    </Link> 
+                </li>))
+            }
+        </ul>
     </div>
   )
 }
